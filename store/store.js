d0b628eacd7ebd4282b2b5a0fb180367150f03af
‘ruijiaFor’ <3522443660@qq.com>
2022-11-05 22:03:44 +0800

    商品总额有问题

---
+++ store/store.js
@@ -0,0 +1,18 @@
+//在这个js文件中，创建store的实例对象
+import {observable,action} from 'mobx-miniprogram'
+
+export const store = observable({
+  //数据字段
+  goodsId:[],
+  goodId:null,
+  goodsNum:null,
+  updateGoodsId:action(function(newId){
+    this.goodId = newId
+  }),
+  addGoods:action(function(newId){
+    this.goodsId = [...this.goodsId,newId]
+  }),
+  addNum:action(function(step){
+    this.goodsNum += step
+  })
+})
--- utils/util.js
+++ utils/util.js
@@ -1,19 +1,11 @@
-const formatTime = date => {
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
-  const hour = date.getHours()
-  const minute = date.getMinutes()
-  const second = date.getSeconds()
-
-  return `${[year, month, day].map(formatNumber).join('/')} ${[hour, minute, second].map(formatNumber).join(':')}`
-}
-
-const formatNumber = n => {
-  n = n.toString()
-  return n[1] ? n : `0${n}`
+const sumMoney =(arr)=>{
+  let sum = 0
+  for(let i =0 ;i<arr.length;i++){
+    sum += arr[i].goods_price
+  }
+  return sum
 }
 
 module.exports = {
-  formatTime
+  sumMoney
 }
