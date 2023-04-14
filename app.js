79407df9e09dcc7148576d578f97dcc65ad44193
hbc <940010004@qq.com>
2023-04-14 12:33:36 +0800

    删除detail页面

--- app.js
+++ app.js
@@ -2,22 +2,22 @@
 App({
   onLaunch() {
     // 展示本地存储能力
-    const logs = wx.getStorageSync("logs") || [];
-    logs.unshift(Date.now());
-    wx.setStorageSync("logs", logs);
+    const logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
 
     // 登录
     wx.login({
-      success: (res) => {
+      success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      },
-    });
+      }
+    })
     wx.setTabBarStyle({
       selectedColor: "pink",
-      borderStyle: "white",
-    });
+      borderStyle: 'white'
+    })
   },
   globalData: {
-    userInfo: null,
-  },
-});
+    userInfo: null
+  }
+})
