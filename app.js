41a0a5a4706efa476a8a8aa0cfab1a374866487f
hbc <940010004@qq.com>
2023-04-14 13:41:07 +0800

    git测试重置

--- app.js
+++ app.js
@@ -1,32 +1,22 @@
-<<<<<<< HEAD
 // app.js
 App({
   onLaunch() {
     // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
+    const logs = wx.getStorageSync("logs") || [];
+    logs.unshift(Date.now());
+    wx.setStorageSync("logs", logs);
     // 登录
     wx.login({
-      success: res => {
+      success: (res) => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
+      },
+    });
     wx.setTabBarStyle({
       selectedColor: "pink",
-      borderStyle: 'white'
-    })
+      borderStyle: "white",
+    });
   },
   globalData: {
-<<<<<<< HEAD
     userInfo: null,
   },
 });
-=======
->>>>>>> dev-gjj
-=======
-    userInfo: null
-  }
-})
->>>>>>> 6dce0c38b35769477c3851e55050402d5914d3b9
